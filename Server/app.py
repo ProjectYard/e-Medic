c942678436539e0c376b4804dd0f15c721b19c55
Ashish Sahu <ashishsahu1999@hotmail.com>
2021-04-14 23:51:49 +0530

    route update

--- Server/app.py
+++ Server/app.py
@@ -1,9 +1,14 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/get')
+def hello_world1():
+    return 'Hello, shristi ruakkar!!'
 
 if __name__ == "__main__":
     app.run(debug=True)
