2c596de2ff25310c286e43e9eaf627775d22bb2a
Ashish Sahu <ashishsahu1999@hotmail.com>
2021-04-15 00:00:37 +0530

    html files added

--- Server/app.py
+++ Server/app.py
@@ -6,9 +6,5 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
-@app.route('/get')
-def hello_world1():
-    return 'Hello, shristi ruakkar!!'
-
 if __name__ == "__main__":
     app.run(debug=True)
